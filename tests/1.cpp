2ab8e1130f5967b1ad58269e278178e6d20fe4af
int2t <2103859514@qq.com>
2025-10-19 13:58:23 +0800

    modify

--- tests/1.cpp
+++ tests/1.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
+// show git modify
 int main()
 {
+    // some operation
+    cout << "Hello, git" << endl;
     cout << "Hello, World!" << endl;
     return 0;
 }
