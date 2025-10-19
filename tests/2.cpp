99eb0419b49e1cd58ed2c02687a0f8b610997388
int2t <2103859514@qq.com>
2025-10-19 13:55:48 +0800

    test2

---
+++ tests/2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+//simple calculator
+int main()
+{
+    float num1, num2;
+    char operation;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+    switch (operation)
+    {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Error: Division by zero is not allowed." << endl;
+            break;
+        default:
+            cout << "Error: Invalid operation." << endl;
+            break;
+    }
+    return 0;
+}
